cba06021881d372203a4b1b2ca685d0b3ad05d47
Gustavo Rivero <gustavoerivero12@gmail.com>
2023-06-22 20:40:11 -0400

    009 - Add init configs to BottomNavigation

---
+++ src/navigation/BottomNavigation.tsx
@@ -0,0 +1,10 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+import colors from '../styled-components/colors'
+
+import ProfilePage from '../pages/ProfilePage'
+import TimelinePage from '../pages/TimelinePage'
+
+import { View, StyleSheet } from 'react-native'
+
+const Tab = createBottomTabNavigator()
