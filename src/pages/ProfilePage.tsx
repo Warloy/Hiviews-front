ac21c7bdcd4fdb0fa3fed794be5b8e283f7a5312
rinzldev <oswaldo.contacto@protonmail.com>
2023-07-09 01:40:22 -0400

    feature: follow and unfollow button

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -11,8 +11,12 @@ interface ProfilePageProps {
 }
 
 const ProfilePage: React.FC<ProfilePageProps> = ({ navigation }) => {
-  const description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur, tortor sed eleifend auctor, nisi arcu fringilla ex, in commodo leo enim nec dolor.";
+  const description = " El front es mi pasión, nunca cambies.";
+  const [isFollowing, setIsFollowing] = useState(false);
 
+  const handleFollowToggle = () => {
+    setIsFollowing(!isFollowing);
+  };
   return (
     <ScrollView>
       <Container
@@ -28,14 +32,24 @@ const ProfilePage: React.FC<ProfilePageProps> = ({ navigation }) => {
             source={{uri: "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"}}
           />
           <View style={{ marginLeft: 16 }}>
-            <Text style={{ fontSize: 24, fontWeight: 'bold', color: colors.gray3 }}>
-              John Doe
-            </Text>
-            <Text style={{ fontSize: 16, color: colors.gray2 }}>
-              @johndoe
-            </Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+              <View style={{ marginRight: 65 }}>
+                <Text style={{ fontSize: 24, fontWeight: 'bold', color: colors.gray3 }}>
+                  John Doe
+                </Text>
+                <Text style={{ fontSize: 16, marginBottom: 12, color: colors.gray2 }}>
+                  @johndoe
+                </Text>
+              </View>
+              <TouchableOpacity onPress={handleFollowToggle} style={{ backgroundColor: isFollowing ? colors.secondary : colors.primary, paddingVertical: 4 ,paddingHorizontal: 13, borderRadius: 4 }}>
+                <Text style={{ fontSize: 14, fontWeight: 'bold', alignItems: 'center', color: colors.button.text }}>
+                  {isFollowing ? 'Siguiendo' : 'Seguir'}
+                </Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
+
 
         <Text style={{ fontSize: 14, textAlign: 'justify', marginHorizontal: 14, marginTop: 18, marginBottom: 14, color: colors.gray5 }}>
           {description}
@@ -43,23 +57,23 @@ const ProfilePage: React.FC<ProfilePageProps> = ({ navigation }) => {
 
         <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 16 }}>
           <TouchableOpacity onPress={() => navigation?.navigate('ChangePasswPage')} style={{ alignItems: 'center' }}>
-            <Text style={{ fontSize: 18, fontWeight: 'bold', color: colors.secondary}}>100</Text>
+            <Text style={{ fontSize: 18, fontWeight: 'bold', color: colors.secondary }}>100</Text>
             <Text style={{ fontSize: 12, color: colors.gray3 }}>Hilos</Text>
           </TouchableOpacity>
           <TouchableOpacity onPress={() => console.log('Reseñas')} style={{ alignItems: 'center' }}>
-            <Text style={{ fontSize: 18, fontWeight: 'bold', color: colors.secondary}}>500</Text>
+            <Text style={{ fontSize: 18, fontWeight: 'bold', color: colors.secondary }}>500</Text>
             <Text style={{ fontSize: 12, color: colors.gray3 }}>Reseñas</Text>
           </TouchableOpacity>
           <TouchableOpacity onPress={() => console.log('Seguidos')} style={{ alignItems: 'center' }}>
-            <Text style={{ fontSize: 18, fontWeight: 'bold', color: colors.secondary}}>1</Text>
+            <Text style={{ fontSize: 18, fontWeight: 'bold', color: colors.secondary }}>1</Text>
             <Text style={{ fontSize: 12, color: colors.gray3 }}>Seguidos</Text>
           </TouchableOpacity>
           <TouchableOpacity onPress={() => console.log('Seguidores')} style={{ alignItems: 'center' }}>
-            <Text style={{ fontSize: 18, fontWeight: 'bold', color: colors.secondary}}>1k</Text>
+            <Text style={{ fontSize: 18, fontWeight: 'bold', color: colors.secondary }}>1k</Text>
             <Text style={{ fontSize: 12, color: colors.gray3 }}>Seguidores</Text>
           </TouchableOpacity>
           <TouchableOpacity onPress={() => console.log('Favoritos')} style={{ alignItems: 'center' }}>
-            <Text style={{ fontSize: 18, fontWeight: 'bold', color: colors.secondary}}>1.7k</Text>
+            <Text style={{ fontSize: 18, fontWeight: 'bold', color: colors.secondary }}>1.7k</Text>
             <Text style={{ fontSize: 12, color: colors.gray3 }}>Favoritos</Text>
           </TouchableOpacity>
         </View>
@@ -69,7 +83,7 @@ const ProfilePage: React.FC<ProfilePageProps> = ({ navigation }) => {
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
           {/* Aquí puedes agregar más contenido si es necesario */}
         </View>
-        
+
       </Container>
     </ScrollView>
   );
