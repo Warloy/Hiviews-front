a80e5d04e5f48e329b79a4c2a828710327ef6734
Jesus M <warloy989@gmail.com>
2023-09-27 09:12:58 -0400

    Added categories object for threads in constants

---
+++ src/constants/Categories.ts
@@ -0,0 +1,24 @@
+const categories = [
+  {
+    id: "1",
+    name: "Cinéfilo"
+  },
+  {
+    id: "2",
+    name: "Discusión"
+  },
+  {
+    id: "3",
+    name: "Memes"
+  },
+  {
+    id: "4",
+    name: "Offtopic"
+  },
+  {
+    id: "5",
+    name: "Trending"
+  },
+]
+
+export default categories
