68599b3c1c8d7d0d1295513e1dc4ab11e6a48c67
Jesus M <warloy989@gmail.com>
2023-09-27 09:16:40 -0400

    Moved categories static data to its proper folder

---
+++ src/static/categoriesData.ts
@@ -0,0 +1,26 @@
+import { TCategory } from "@/types/Post.Type";
+
+const categoriesData: TCategory[] = [
+  {
+    id: 1,
+    name: "Cinéfilo"
+  },
+  {
+    id: 2,
+    name: "Discusión"
+  },
+  {
+    id: 3,
+    name: "Memes"
+  },
+  {
+    id: 4,
+    name: "Offtopic"
+  },
+  {
+    id: 5,
+    name: "Trending"
+  },
+]
+
+export default categoriesData
